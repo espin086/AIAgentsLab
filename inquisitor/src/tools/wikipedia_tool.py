47f733fea6a8c4fb954af3cbb5159acf9f70434a
JJ <4843736+espin086@users.noreply.github.com>
2025-01-07 17:53:58 -0800

    feature: added wikipedia tool

---
+++ inquisitor/src/tools/wikipedia_tool.py
@@ -0,0 +1,21 @@
+"""
+This module contains the Wikipedia tool for the Inquisitor project.
+"""
+
+from langchain_community.tools import WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
+
+LENGHT_OF_RESPONSE = 1000
+
+wiki_api_wrapper = WikipediaAPIWrapper(
+    top_k_results=1, doc_content_chars_max=LENGHT_OF_RESPONSE
+)
+wikipedia = WikipediaQueryRun(
+    description="""A tool to explain things in text format. 
+    Use this tool if you think the user's asked concept is best explained dthrough text""",
+    api_wrapper=wiki_api_wrapper,
+)
+
+
+if __name__ == "__main__":
+    print(wikipedia.invoke("What is a mobius strip?"))
